d44b7e7a37b5e5ee29dfa70e96688bea46bc052e
jsdnlb <danielb.alzate1@gmail.com>
2022-04-04 08:29:09 -0500

    How to get a client body and change response status code

--- basic-rest-api/app.py
+++ basic-rest-api/app.py
@@ -1,14 +1,32 @@
-import json, falcon
+import json
+import falcon
+
 
 class ObjectRequestClass:
     def on_get(self, req, res):
-        data = {
-            'name' : 'Daniel',
+        res.status = falcon.HTTP_200
+
+        data = json.loads(req.stream.read())
+
+        content = {
+            'name': 'Daniel',
             'lastname': 'Buitrago',
-            'role' : 'admin'
+            'role': 'admin'
         }
 
-        res.body = json.dumps(data)
+        output = {}
+        if 'method' not in data:
+            res.status = falcon.HTTP_501
+            output['error'] = 'Error: none method found'
+        else:
+
+            if data['method'] == 'get-name':
+                output['name'] = content['name']
+            else:
+                res.status = falcon.HTTP_400
+                output['name'] = None
+
+        res.media = output
         print('Run ObjectRequestClass...')
 
 
